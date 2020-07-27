773439dbb58a1ca9b9a09876b6849dffe657a8a4
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-27 15:13:02 -0300

    focus node

--- lib/pages/form.dart
+++ lib/pages/form.dart
@@ -8,10 +8,15 @@ class FormularioCadastro extends StatelessWidget {
   final _controllerEmail = TextEditingController();
   final _controllerCPF = TextEditingController();
 
-  _form(String text, String hint, 
+  final _focusLogin = FocusNode();
+  final _focusEmail = FocusNode();
+  final _focusSenha = FocusNode();
+  final _focusCPF = FocusNode();
+
+  _form(BuildContext context, text, String hint,
   {obscure = false, TextEditingController controller,
   FormFieldValidator<String> validator, TextInputType keyboardType,
-  int maxLength}){
+  int maxLength, TextInputAction textInputAction, FocusNode focusNode, ValueChanged<String> onFieldSubmitted}){
     return TextFormField(
       controller: controller,
       validator: validator,
@@ -19,6 +24,9 @@ class FormularioCadastro extends StatelessWidget {
       keyboardType: keyboardType,
       maxLength: maxLength,
       maxLines : 1,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      onFieldSubmitted: onFieldSubmitted,
       decoration: InputDecoration(
         labelText: text,
         hintText: hint,
@@ -34,7 +42,7 @@ class FormularioCadastro extends StatelessWidget {
   }
   _button(String text, Function onPressed){
     return Container(
-      height: 40  ,
+      height: 40,
       child: RaisedButton(
         child: Text(text, style: TextStyle(fontSize: 18, color: Colors.white)),
         color: Colors.blue,
@@ -59,39 +67,59 @@ class FormularioCadastro extends StatelessWidget {
     }
   }
 
-  _body(){
+  _body(context){
     return Form(
       key: _formKey,
       child: Container(
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: <Widget>[
-            _form('Nome', 'Informe seu nome', 
-            controller: _controllerNome, validator: (String nome){
+            _form(context, 'Nome', 'Informe seu nome',
+            textInputAction: TextInputAction.next,
+            controller: _controllerNome,
+            onFieldSubmitted: (String text){
+              FocusScope.of(context).requestFocus(_focusLogin);
+            },
+            validator: (String nome){
               if(nome.isEmpty){
                 return 'Preencha seu nome';
               }
               return null;
             }),
             SizedBox(height: 20,),
-            _form('Login', 'Login de acesso', 
-            controller: _controllerLogin, validator: (String login){
+            _form(context, 'Login', 'Login de acesso',
+            controller: _controllerLogin,
+            focusNode: _focusLogin,
+            onFieldSubmitted: (String text){
+              FocusScope.of(context).requestFocus(_focusEmail);
+            },
+            validator: (String login){
               if(login.isEmpty){
                 return 'Preencha o campo';
               }
               return null;
             }),
             SizedBox(height: 20,),
-                        _form('Email', 'Informe seu email', keyboardType: TextInputType.emailAddress, 
-            controller: _controllerEmail, validator: (String email){
+            _form(context, 'Email', 'Informe seu email', keyboardType: TextInputType.emailAddress, 
+            controller: _controllerEmail,
+            focusNode: _focusEmail,
+            onFieldSubmitted: (String text){
+              FocusScope.of(context).requestFocus(_focusSenha);
+            },
+            validator: (String email){
               if(email.isEmpty){
                 return 'Informe seu email';
               }
               return null;
             }),
             SizedBox(height: 20,),
-            _form('Senha', 'Informe uma senha', obscure: true,
-            controller: _controllerSenha, validator: (String senha){
+            _form(context, 'Senha', 'Informe uma senha', obscure: true,
+            controller: _controllerSenha,
+            focusNode: _focusSenha,
+            onFieldSubmitted: (String text){
+              FocusScope.of(context).requestFocus(_focusCPF);
+            },
+            validator: (String senha){
               if(senha.isEmpty){
                 return 'Informe uma senha';
               } else if(senha.length < 8){
@@ -100,8 +128,19 @@ class FormularioCadastro extends StatelessWidget {
               return null;
             }),
             SizedBox(height: 20,),
-            _form('CPF: ', 'Informe seu CPF', keyboardType: TextInputType.number,
-            controller: _controllerCPF, maxLength: 11),
+            _form(context, 'CPF: ', 'Apenas números', keyboardType: TextInputType.number,
+            controller: _controllerCPF,
+            maxLength: 11,
+            focusNode: _focusCPF,
+            validator: (String cpf){
+              if(cpf.isEmpty){
+                return 'Informe seu CPF';
+              } else if(cpf.length != 11){
+                return 'CPF inválido';
+              }
+              return null;
+            }),
+            SizedBox(height: 20,),
             _button('Cadastrar', _onClick),
           ],
         ),
@@ -115,7 +154,7 @@ class FormularioCadastro extends StatelessWidget {
         title: Text('Cadastro'),
         centerTitle: true,
       ),
-      body: _body(),
+      body: _body(context),
     );
   }  
 }
